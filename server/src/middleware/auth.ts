08f6e832faee7e886ee1fa19ba65819bf381a26b
slvonderheide <slvonderheide@gmail.com>
2025-03-15 13:38:10 -0400

    initial commit

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+  username: string;
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      return next();
+    });
+  } else {
+    return res.sendStatus(401);}
+  };
