8b9c046a2630f79e42da32998ef5380fada7cbc3
slvonderheide <slvonderheide@gmail.com>
2025-03-16 14:47:05 -0400

    created authAPI, Auth.ts, index.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,10 +2,14 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
     // TODO: return the decoded token
   }
 
   loggedIn() {
+    const token = this.getToken();
+    return loggedUser;
     // TODO: return a value that indicates if the user is logged in
   }
   
@@ -14,15 +18,21 @@ class AuthService {
   }
 
   getToken(): string {
+    const loggedUser = localStorage.getItem('id_token');
+    return loggedUser;
     // TODO: return the token
   }
 
   login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
   }
 
   logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
   }
